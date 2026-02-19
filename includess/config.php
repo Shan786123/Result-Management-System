19923269370f993978740f2b731416716d2549d5
Shan Khan <bhaibiology66@gmail.com>
2026-02-19 16:05:29 +0530

    SOmething

--- includess/config.php
+++ includess/config.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'rehan');
-define('BASE_URL', 'http://localhost/rehan/');
+define('DB_HOST', 'sqlxxx.infinityfree.com');
+define('DB_USER', 'if0_41195576');
+define('DB_PASS', 'JYgZoqxqnsxkBf');
+define('DB_NAME', 'epiz_rms_db');
+
 
 try {
     $pdo = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_NAME, DB_USER, DB_PASS);
